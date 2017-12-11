647d773a935323bcdb5f7d9b8363a51bad9b6cf7
NISHI3 <ryonishi3@gmail.com>
2017-12-11 09:58:52 +0900

    update 12/11朝

--- js/pages/timetable.js
+++ js/pages/timetable.js
@@ -95,6 +95,14 @@ Vue.component('fixed-area', {
   },
 });
 
+const toSeason = month => {
+  month = (typeof month === 'number') ? month : ((new Date()).getMonth() + 1);
+  return (3 <= month && month <= 8) ? 0 : 1;
+};
+
+const FIRST_SEASON = 1;
+const SECOND_SEASON = 2;
+
 Vue.component('timetable', {
   template: `
     <div class="table-responsive">
@@ -142,34 +150,116 @@ new Vue({
       firstMaxCredit: 26,
       secondMaxCredit: 26,
       subjects: [],
+      currentSeason: 0,
+      grade: 2,
       originalTimetables: [],
     },
-  created: function () {
-    //　ライフサイクルフック(インスタンスが生成された後)
-    var subjectPromise = $.getJSON('../js/json/subject.json');
-    var optionPromise = $.getJSON('../js/json/account.json');
-    $.when(subjectPromise, optionPromise).then(([subjects], [option]) => {
-      this.subjects = subjects.filter(subject => {
-        return subject.state !== 3;
+    created: function () {
+      //　ライフサイクルフック(インスタンスが生成された後)
+      var subjectPromise = $.getJSON('../js/json/subject.json');
+      var optionPromise = $.getJSON('../js/json/account.json');
+      console.log(subjectPromise);
+      $.when(subjectPromise, optionPromise).then(([subjects], [option]) => {
+        this.subjects = subjects.filter(subject => {
+          return subject.state !== 3;
+        });
+        // Save用のやつ
+        // this.originalTimetables = this.subjects
+        //   .map(subject => subject.timetable)
+        //   .reduce((memo, timetables) => {
+        //     timetables = timetables.map(timetable => {
+        //       return {
+        //         active: !!timetable.active,
+        //         subject_id: timetable.subject_id
+        //       };
+        //     });
+        //     return memo.concat(timetables);
+        //   }, []);
+        console.log(this.subjects);
+        this.firstMaxCredit = option.first_cap_over;
+        this.secondMaxCredit = option.second_cap_over;
       });
-      this.originalTimetables = this.subjects
-        .map(subject => subject.timetable)
-        .reduce((memo, timetables) => {
-          timetables = timetables.map(timetable => {
-            return {
-              active: !!timetable.active,
-              subject_id: timetable.subject_id
-            };
-          });
-          return memo.concat(timetables);
-        }, []);
-      this.firstMaxCredit = option.first_cap_over;
-      this.secondMaxCredit = option.second_cap_over;
-    });
-  },
-  methods: {
+    },
+    methods: {
 
-  },
+    },
+    computed: {
+      firstCredit() {
+        return this.subjects.reduce((memo, subject) => {
+          const includedCap = subject.included_cap;
+          const anyActive = subject.timetable.some(timetable => {
+            return timetable.season === FIRST_SEASON &&
+              timetable.active;
+          })
+          if (includedCap && anyActive)
+            memo += subject.credit;
+            console.log(memo);
+          return memo;
+        }, 0);
+      },
+      secondCredit() {
+        return this.subjects.reduce((memo, subject) => {
+          const includedCap = subject.included_cap;
+          const anyActive = subject.timetable.some(timetable => {
+            return timetable.season === SECOND_SEASON &&
+              timetable.active;
+          })
+          if (includedCap && anyActive)
+            memo += subject.credit;
+          return memo;
+        }, 0);
+      },
+      totalCredit() {
+        return this.firstCredit + this.secondCredit;
+      },
+      totalMaxCredit() {
+        return this.firstMaxCredit + this.secondMaxCredit - 4;
+      },
+
+      // 単位数オーバーの算出
+      firstCreditOver() {
+        return this.firstCredit > this.firstMaxCredit;
+      },
+      secondCreditOver() {
+        return this.secondCredit > this.secondMaxCredit;
+      },
+      totalCreditOver() {
+        return this.totalCredit > this.totalMaxCredit;
+      },
+      creditOver() {
+        return this.firstCreditOver || this.secondCreditOver || this.totalCreditOver;
+      },
+      timetableStateArray() {
+        return this.subjects
+          .map(subject => subject.timetable)
+          .reduce((memo, timetables) => {
+            timetables = timetables.map(timetable => {
+              return {
+                active: !!timetable.active,
+                subject_id: timetable.subject_id
+              };
+            });
+            return memo.concat(timetables);
+          }, []);
+      },
+      changeState() {
+        return !_.isEqual(this.originalTimetables, this.timetableStateArray);
+      },
+      disabledButton() {
+        // CAPオーバーしている場合や
+        // 元の配列と同一な場合は保存ボタンを有効にする必要がない
+        return this.creditOver || !this.changeState;
+      }
+    },
+    filters: {
+      season(subjects, season = 0) {
+        return subjects.map(subject => {
+          return subject.timetable.filter(timetable => {
+            return timetable.season === season;
+          });
+        }).reduce((res, timetables) => res.concat(timetables), []);
+      }
+    },
     components: {
       'color-meaming': colorMeaming,
       // 'fixed-area': fixedArea
